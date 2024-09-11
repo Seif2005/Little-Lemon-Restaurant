36b3c4d088252da3bf84e7d449b8d586e814354f
seif <seif.m05@gmail.com>
2024-09-11 13:01:43 +0300

    banner

--- src/components/HomeCenter/HomeCenter.js
+++ src/components/HomeCenter/HomeCenter.js
@@ -1,13 +1,19 @@
 import "./HomeCenter.css"
+import Button from "../../Button/Button";
 
 function HomeCenter(){
     return(
         <div className="main-part">
+            <section className="column">
+                <article className="text-area">
+                <h1 className="restaurant-title">Little Lemon</h1>
+                <h3 className="chicago">Chicago</h3>
+                <h5 className="lower-text">We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</h5>
+                <Button text = "Reserve a Table" action="/reserve"/>
+                </article>
+            </section>
             <article className="column">
-                <h1>Little Lemon Restaurant</h1>
-            </article>
-            <article className="column">
-                <img src="../../assets/restauranfood.jpg" alt = "Main Image" className="main-image"/>
+                <img src="../../assets/restauranfood.jpg" alt = "Main Image" className="main"/>
             </article>
 
         </div>
