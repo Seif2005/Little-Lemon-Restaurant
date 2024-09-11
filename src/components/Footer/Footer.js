3110e61f79d6e6a04ae0f85a3282d3f0c9b1b528
seif <seif.m05@gmail.com>
2024-09-11 18:31:57 +0300

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,34 @@
+import "./Footer.css"
 function Footer(){
     return(
         <>
-        <h1>Footer</h1>
+        <section className="footer-section">
+            <section className="footer-column">
+                <img src="logo.png"/>
+            </section>
+            <section className="footer-column">
+                <h5 className="footer-title">App Navigation</h5>
+                <h6 className="footer-data">Home</h6>
+                <h6 className="footer-data">About</h6>
+                <h6 className="footer-data">Menu</h6>
+                <h6 className="footer-data">Reservation</h6>
+                <h6 className="footer-data">Order Online</h6>
+                <h6 className="footer-data">Login</h6>
+            </section>
+            <section className="footer-column contact-section">
+            <h5 className="footer-title">Contact</h5>
+            <h6 className="footer-data">Address</h6>
+            <h6 className="footer-data">Phone Number</h6>
+            <h6 className="footer-data">Email</h6>
+
+            </section>
+            <section className="footer-column contact-section">
+            <h5 className="footer-title">Social Media</h5>
+            <h6 className="footer-data">Facebook</h6>
+            <h6 className="footer-data">Instagram</h6>
+            <h6 className="footer-data">Twitter</h6>
+            </section>
+        </section>
         </>
     );
 }
